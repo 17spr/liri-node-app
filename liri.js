bb3af20b43538252309b5ad9923721e68165c3ed
jesse <jessemspringer@gmail.com>
2018-02-19 22:23:45 -0500

    got Twitter working, now on to Spotify...

--- liri.js
+++ liri.js
@@ -1,30 +1,81 @@
-// Twitter API =======================================
 require("dotenv").config();
+
+// importing API keys from key.js file
 var myKeys = require("./keys.js")
 
+// Spotify API ======================================
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(myKeys.spotify);
+var spotifyThisSong = process.argv[2];
+var songName = process.argv[3];
+
+if (spotifyThisSong === 'spotify-this-song') {
+  console.log("Working on it...")
+  getSong();
+}
+
+function getSong() {
+  spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
+}
 
 
 
+// Twitter API =======================================
 
+// creating a variable to capture and store the `my-tweets` argument
+var myTweets = process.argv[2];
+
+// if-then logic for making the Twitter API call
+if (myTweets === 'my-tweets') {
+  console.log("Fetching tweets...")
+  // calling the function that retrieves my tweets
+  getTweets();
+}
+// creating a function that performs all of the code for getting my tweets using the Twitter API
+function getTweets() {
+  var Twitter = require('twitter');
+ 
+  var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+  
+  var params = {screen_name: 'Alias_Jesse'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      // looping through response array that contains each tweet to access the text and the creation date
+      for (i = 0; i < tweets.length; i++) {
+        // displaying the results in the console 
+        console.log(tweets[i].text);
+        console.log(tweets[i].created_at);
+      }
+    }
+  });
+  
+}
 // OMDB API ==========================================
 
-// Notes..............................................
+// ***If the movie name has multiple words (for example: How the Grinch Stole Christmas), write the name in quotes. Otherwise it won't work.****
 // working example of using a multi-word movie name:
          // node .\liri.js movie-this "how the grinch stole christmas"
-// single-word movie name: 
+// single-word movie name example: 
          // node .\liri.js movie-this cinderella
-// IMPORTANT:
-// ***If the movie name has multiple words (i.e. How the Grinch Stole Christmas), write the name in quotes. Otherwise it won't work.****
-// .....................................................
+// ...................................................
 
-
-// variable to store `movie-this` command 
+// variable to store `movie-this` argument 
 var movieThis = process.argv[2];
-// variable to store the name of the movie
+// variable to store the movie name argument
 var movieName = process.argv[3];
 
 // if-then logic for performing the API request for OMDB movie data...
-
 if (movieThis === 'movie-this' && movieName) {
   console.log("It's working!")
   // executing the function to get movie data
@@ -35,21 +86,19 @@ if (movieThis === 'movie-this' && movieName) {
   console.log("Fine then, let's learn about Mr. Nobody.")
   mrNobody();
 
-} else if (movieThis != true) {
-  console.log("You must type `movie-this` before typing the movie name. For example: node liri.js movie-this cinderella")
-}
+} 
 // creating the function that performs the OMDB API request
 function getMovie() {
   var request = require("request");
+  // concatenating the url parameters
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-console.log("URL: " + queryUrl);
-
-request(queryUrl, function(error, response, body) {
+  console.log("URL: " + queryUrl);
+  // making the API request...
+  request(queryUrl, function(error, response, body) {
   
   if (!error && response.statusCode === 200) {
-    // logging the JSON response for the data required in the homework description (title, release year, etc.)
+    // logging the data required in the homework description (title, release year, etc.)
     console.log("Title: " + JSON.parse(body).Title);
     console.log("Release Year: " + JSON.parse(body).Year);
     console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
@@ -63,20 +112,15 @@ request(queryUrl, function(error, response, body) {
 });
   
 }
-
 // function to retrieve data for 'Mr. Nobody' in case no movie name is specified
 function mrNobody() {
-
   var mrNobodyMovie = "mr nobody";
-
   var request = require("request");
-
   var queryUrl = "http://www.omdbapi.com/?t=" + mrNobodyMovie + "&y=&plot=short&apikey=trilogy";
 
   console.log("URL: " + queryUrl);
 
   request(queryUrl, function(error, response, body) {
-    // handling errors
     if (!error && response.statusCode === 200) {
       // logging the JSON response for the data required in the homework description (title, release year, etc.)
       console.log("Title: " + JSON.parse(body).Title);
@@ -87,14 +131,7 @@ function mrNobody() {
       console.log("Language: " + JSON.parse(body).Language);
       console.log("Plot: " + JSON.parse(body).Plot);
       console.log("Actors: " + JSON.parse(body).Actors);
-
     }
   });
   
 };
-
-
-
-
-
-
